b3b1cd54a83faf62573541a59519214d77b08b96
Muhammad Dzakwan Hanif <dzakwanhanif47@gmail.com>
2026-09-19 15:03:15 +0700

    ✅ Binary Tree Nodes | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-09-19

--- hackerrank/medium/binary-search-tree-1/solution.sql
+++ hackerrank/medium/binary-search-tree-1/solution.sql
@@ -1,16 +1,10 @@
 /*
 Enter your query here.
-*/SELECT 
-    MAX(CASE WHEN OCCUPATION = 'Doctor' THEN NAME END) AS Doctor,
-    MAX(CASE WHEN OCCUPATION = 'Professor' THEN NAME END) AS Professor,
-    MAX(CASE WHEN OCCUPATION = 'Singer' THEN NAME END) AS Singer,
-    MAX(CASE WHEN OCCUPATION = 'Actor' THEN NAME END) AS Actor
-FROM (
-    SELECT 
-        NAME, 
-        OCCUPATION, 
-        ROW_NUMBER() OVER(PARTITION BY OCCUPATION ORDER BY NAME) AS row_num
-    FROM OCCUPATIONS
-) AS temp
-GROUP BY row_num
-ORDER BY row_num;
+*/SELECT N,
+    CASE 
+        WHEN P IS NULL THEN 'Root'
+        WHEN N IN (SELECT DISTINCT P FROM BST WHERE P IS NOT NULL) THEN 'Inner'
+        ELSE 'Leaf'
+    END
+FROM BST
+ORDER BY N ASC;
