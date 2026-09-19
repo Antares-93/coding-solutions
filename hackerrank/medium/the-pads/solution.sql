54c73c496583d5cc331b59fdfe8f7134d691e920
Muhammad Dzakwan Hanif <dzakwanhanif47@gmail.com>
2026-09-19 14:55:40 +0700

    ✅ The PADS | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-09-19

--- hackerrank/medium/the-pads/solution.sql
+++ hackerrank/medium/the-pads/solution.sql
@@ -1,10 +1,10 @@
 /*
 Enter your query here.
-*/SELECT 
-    CASE 
-        WHEN A + B <= C OR A + C <= B OR B + C <= A THEN 'Not A Triangle'
-        WHEN A = B AND B = C THEN 'Equilateral'
-        WHEN A = B OR B = C OR A = C THEN 'Isosceles'
-        ELSE 'Scalene'
-    END
-FROM TRIANGLES;
+*/SELECT CONCAT(NAME, '(', SUBSTR(OCCUPATION, 1, 1), ')')
+FROM OCCUPATIONS
+ORDER BY NAME ASC;
+
+SELECT CONCAT('There are a total of ', COUNT(OCCUPATION), ' ', LOWER(OCCUPATION), 's.')
+FROM OCCUPATIONS
+GROUP BY OCCUPATION
+ORDER BY COUNT(OCCUPATION) ASC, OCCUPATION ASC;
