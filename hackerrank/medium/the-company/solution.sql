a600d2e0252e92bd9d8c244fb1f0b9584261f469
Muhammad Dzakwan Hanif <dzakwanhanif47@gmail.com>
2026-09-19 15:05:22 +0700

    ✅ New Companies | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-09-19

--- hackerrank/medium/the-company/solution.sql
+++ hackerrank/medium/the-company/solution.sql
@@ -1,10 +1,16 @@
 /*
 Enter your query here.
-*/SELECT N,
-    CASE 
-        WHEN P IS NULL THEN 'Root'
-        WHEN N IN (SELECT DISTINCT P FROM BST WHERE P IS NOT NULL) THEN 'Inner'
-        ELSE 'Leaf'
-    END
-FROM BST
-ORDER BY N ASC;
+*/SELECT 
+    c.company_code, 
+    c.founder, 
+    COUNT(DISTINCT lm.lead_manager_code), 
+    COUNT(DISTINCT sm.senior_manager_code), 
+    COUNT(DISTINCT m.manager_code), 
+    COUNT(DISTINCT e.employee_code)
+FROM Company c
+JOIN Lead_Manager lm ON c.company_code = lm.company_code
+JOIN Senior_Manager sm ON lm.lead_manager_code = sm.lead_manager_code
+JOIN Manager m ON sm.senior_manager_code = m.senior_manager_code
+JOIN Employee e ON m.manager_code = e.manager_code
+GROUP BY c.company_code, c.founder
+ORDER BY c.company_code ASC;
