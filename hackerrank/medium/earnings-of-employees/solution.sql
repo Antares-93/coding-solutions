7505fca14da4d5d3936110124451bd8f1f6a72d2
Muhammad Dzakwan Hanif <dzakwanhanif47@gmail.com>
2026-09-22 11:29:48 +0700

    ✅ Top Earners | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-09-22

--- hackerrank/medium/earnings-of-employees/solution.sql
+++ hackerrank/medium/earnings-of-employees/solution.sql
@@ -1,4 +1,8 @@
 /*
 Enter your query here.
-*/SELECT CEIL(AVG(Salary) - AVG(REPLACE(Salary, '0', '')))
-FROM EMPLOYEES;
+*/
+SELECT (months * salary) AS earnings, COUNT(*)
+FROM Employee
+GROUP BY earnings
+ORDER BY earnings DESC
+LIMIT 1;
